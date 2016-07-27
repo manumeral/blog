86a2f7f1a066cc58898c992b3d48fad6e94ee5cc
Mayank <kumar.mayank98@gmail.com>
2016-07-27 16:45:20 +0530

    Blog First Wokibng Skeleton

--- The_Nomad/blog/models.py
+++ The_Nomad/blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(timezone.now)
+    created_date = models.DateTimeField(timezone.now())
     published_date = models.DateTimeField(blank = True,null = True)
     
     def published(self):
