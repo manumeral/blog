cfcfe17d9f1de16467a63fa40c04048747ac4b42
manumeral <kumar.mayank98@gmail.com>
2016-08-08 03:54:05 +0530

    Postgresql Support and Heroku Support Added

--- The_Nomad/The_Nomad/settings.py
+++ The_Nomad/The_Nomad/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'The_Nomad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'mayank',
+        'PASSWORD': '240410109394@Easy',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -120,3 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
